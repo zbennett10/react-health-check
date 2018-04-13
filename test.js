3507eba47aba5eda2238e6de051dbcdc2fc7502e
Zachary Bennett <zbennett10@hotmail.com>
2018-04-12 21:39:33 -0700

    added test for number of rendered status rows

--- test.js
+++ test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import {configure} from 'enzyme';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import {default as Adapter} from 'enzyme-adapter-react-16';
 import HealthCheck from './src/index';
 
 configure({adapter: new Adapter()});
+
+const testEndPoints = [{
+            url: 'http://localhost/',
+            resourceName: "one"
+        },
+        {
+            url: 'http://localhost/',
+            resourceName: "two",
+            mayReceiveWarnings: true
+        },
+        {
+            url: 'http://localhost/',
+            resourceName: "three"
+    }];
 
 
 test("Health Check renders an overall status view.", () => {
@@ -24,4 +38,17 @@ test("Health Check doesn't render a detailed status view unless clicked.", () =>
     component.find(".rhc-overall-status-view").simulate("click");
 
     expect(component.find(".rhc-detailed-status-view").length).toBe(1);
+});
+
+test("Health Check contains a status, table row for each endpoint when clicked.", () => {
+    const component = mount(
+        <HealthCheck endpoints={[...testEndPoints]} interval={5000}/>
+    );
+
+    expect(HealthCheck.prototype.componentDidMount.calledOnce).toBeTruthy;
+    component.find(".rhc-overall-status-view").simulate("click");
+
+    setTimeout(function() {
+        expect(component.find("tbody tr").length).toBe(3);
+    }, 1000);
 });
